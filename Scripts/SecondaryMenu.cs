6a40acdd850a4d0b06ea128161ea5f4eb7fe029a
Somesh Kale <gkale.somesh@gmail.com>
2018-09-19 09:47:24 +0530

    Add files via upload

---
+++ Scripts/SecondaryMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecondaryMenu : MonoBehaviour {
+
+	public void SecondGame()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 2);
+	}
+}
---
+++ Scripts/learnmenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class learnmenu : MonoBehaviour {
+	public void level()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
